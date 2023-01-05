d6917367c9f6b1a957d870a99c40b4b7fe1033f0
AilinNavas <navasailin18@gmail.com>
2023-01-05 12:21:38 +0100

    Termine funcion y agregue funcion para seleccionar

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-console.log("Hola mundo, aprendiendo a usar GitHub");
+console.log ("Hola mundo, aprendiendo a usar GitHub");
 
 let colores = ["Azul", "Amarillo" ,"Rojo"];
 colores.push("Verde");
 
 console.log(colores);
 
-function recorrerArray (){
-    colores.forEach(function(color){
-        return color
-    });
-}
+function recorrerArray (array){
+    array.forEach(function(elemento, indice){
+        console.log(indice +' - ' + elemento)
+    })
+};
+recorrerArray(colores);
+
+let colorSeleccionado = "Rojo";
+
+function seleccionarColor(array){
+    array.filter(function(elemento){
+    if(elemento === colorSeleccionado){
+        return console.log(elemento);
+    };
+    })
+    };
+
+seleccionarColor(colores);
